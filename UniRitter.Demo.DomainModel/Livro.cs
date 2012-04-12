685d32e6c144990882c2cbb366826a980c08d781
Diogo <diogo.lucas@gmail.com>
2012-04-12 00:06:59 -0300

    Autocomplete implementado

--- UniRitter.Demo.DomainModel/Livro.cs
+++ UniRitter.Demo.DomainModel/Livro.cs
@@ -12,16 +12,22 @@
         [StringLength(100, MinimumLength = 1)]
         public string Nome { get; set; }
 
-        [StringLength(50, MinimumLength = 1)]
+        [StringLength(50)]
         public string Fonte { get; set; }
 
         public int AnoPublicacao { get; set; }
 
-        [StringLength(40, MinimumLength = 1)]
+        [StringLength(40)]
         public string Editora { get; set; }
 
         public Autor Autor { get; set; }
 
         public Genero Genero { get; set; }
+
+        [ForeignKey("Autor")]
+        public int AutorId { get; set; }
+
+        [ForeignKey("Genero")]
+        public int GeneroId { get; set; }
     }
 }
