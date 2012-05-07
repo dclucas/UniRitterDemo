b4ced1dc13e4339bba9b8cd384d26871095e47ce
dlucas <diogo.lucas@gmail.com>
2012-05-07 12:37:38 -0300

    Consertando um problema do carregamento de módulos.

--- UniRitterDemo/App_Start/NinjectWebCommon.cs
+++ UniRitterDemo/App_Start/NinjectWebCommon.cs
@@ -53,7 +53,7 @@ namespace UniRitterDemo.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-            kernel.Load("*.dll");
+            kernel.Load("UniRitter.Demo.*.dll");
         }        
     }
 }
